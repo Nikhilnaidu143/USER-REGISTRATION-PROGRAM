9dd25efae26e3ff23c87f556877acdb82ea4c934
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-12 07:17:08 +0530

    START:- Displayed Welcome Message.

---
+++ src/com/user/registration/problem/UserRegistrationProblem.java
@@ -0,0 +1,8 @@
+package com.user.registration.problem;
+
+public class UserRegistrationProblem {
+
+	public static void main(String[] args) {
+		System.out.println("------------WELCOME TO USER REGISTRATION PROBLEM---------------");
+	}
+}
