2fc9f79a81e3eb7696c773e94e19679162f5386b
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-12 07:22:12 +0530

    UC-1 :- Validated First Name using Regex.

--- src/com/user/registration/problem/UserRegistrationProblem.java
+++ src/com/user/registration/problem/UserRegistrationProblem.java
@@ -1,8 +1,31 @@
 package com.user.registration.problem;
 
-public class UserRegistrationProblem {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class UserRegistrationProblem {
+	static Scanner input = new Scanner(System.in);
+
+	/*** UC-1 :- As a User need to enter a valid First Name. ***/
+	public static void firstNameValidation() {
+		System.out.print("Enter First Name :- ");
+		String first_name = input.nextLine();
+		
+		String firstNameRegex = "^[A-Z][A-Z a-z]{3,}$";           //first character is Upper case an remaining all either Upper or Lower 
+		
+		Pattern pattern = Pattern.compile(firstNameRegex);
+		
+		Matcher matcher = pattern.matcher(first_name);
+		
+		System.out.println("First name pattern matching :- " + matcher.matches());
+	}
+	
+	/**** Main Method ****/
 	public static void main(String[] args) {
-		System.out.println("------------WELCOME TO USER REGISTRATION PROBLEM---------------");
+		/*** Displayed Welcome Message ***/
+		System.out.println("---------------WELCOME TO USER REGISTRATION PROBLEM---------------\n");
+	
+		firstNameValidation();
 	}
 }
